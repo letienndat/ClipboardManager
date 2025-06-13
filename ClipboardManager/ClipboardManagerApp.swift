a9c242bc893604cdc237e6884cc6e2281a8d9fe3
dat.lt <dat.lt@extremevn.com.vn>
2025-06-13 15:19:14 +0700

    Initial Commit

---
+++ ClipboardManager/ClipboardManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipboardManagerApp.swift
+//  ClipboardManager
+//
+//  Created by Le Tien Dat on 6/13/25.
+//
+
+import SwiftUI
+
+@main
+struct ClipboardManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
